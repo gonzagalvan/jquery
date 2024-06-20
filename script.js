8a99c5104244c4cd2cd825b54354f68e2a424aa4
Gonzalo Galvan <gonzalongalvan2@gmail.com>
2024-06-19 23:05:07 -0300

    h1

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 $(document).ready(function() {
     $('#boton1').click(function() {
         $('#imagen').hide();
-        $('.titulo').text("¡No tenemos ninguna imagen!");
+        $('.h1').text("¡No tenemos ninguna imagen!");
     });
     $('#boton2').click(function() {
        $('#imagen').show();
-       $('.titulo').text("¡Mirá la siguiente imagen!");
+       $('.h1').text("¡Mirá la siguiente imagen!");
     })
 });
